79138f17c6cd517a8dc63fe407f6352aab7399cf
Valery <valera.sysov@gmail.com>
2022-05-16 09:27:43 +0300

    0.1 - Start project. Adding gitignore file, project file, CoreData

---
+++ BluetoothFinder/BluetoothFinder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BluetoothFinder
+//
+//  Created by Valera Sysov on 16.05.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
